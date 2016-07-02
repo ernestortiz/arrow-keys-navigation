e12d3331df2318462de66741234acf3e0e70cb0e
Ernesto <ernestortiz@users.noreply.github.com>
2016-07-02 15:09:29 +0200

    Add files via upload

--- js/queries.js
+++ js/queries.js
@@ -1,1 +1,32 @@
+jQuery(document).ready(function ($) {
 
+
+    $(document).on('keydown',function(e) {
+
+        /*if (e.keyCode == 27)
+            msg = 'Esc key pressed. GO TO FIRST post';
+        if ( e.ctrlKey && ( e.which === 27 ) )
+            msg = "You pressed CTRL + Esc: QUIT THE PLUGIN" ;*/
+
+        var kpress = e.keyCode;
+        if (kpress == 37 || kpress == 39 ) {
+            //37 'Left arrow key pressed. GO PREVIOUS post';
+            //39 'Right arrow key pressed. GO NEXT post';
+
+            /**** AJAX request ****/
+            var wildcard = $('#akeynav_wildcard').text();
+            $.ajax({
+                url: ajaxurl,
+                type:'POST',
+                data: { action: 'akeynav_reqs', todo: 'go_post', kpress: kpress, wildcard: wildcard },
+                success:function(results){
+                    if (results)
+                        document.location.href = results;
+                }
+            });
+
+        }
+
+    });
+
+});
